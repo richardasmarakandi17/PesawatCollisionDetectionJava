9660b9be2f9a2ee7ea474dd33eb730b762d9475e
Richard Asmarakandi <soraasmarakandi@gmail.com>
2020-12-11 15:34:24 +0700

    Mari membuat project seperti game yang memanfaatkan collision dengan Java

---
+++ Collision Detection/src/id/ac/its/richard0017/syafiq0089/afifan0234/CollisionEx.java
@@ -0,0 +1,34 @@
+package id.ac.its.richard0017.syafiq0089.afifan0234;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class CollisionEx extends JFrame {
+	
+	public CollisionEx() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Collision");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(() -> {
+            CollisionEx ex = new CollisionEx();
+            ex.setVisible(true);
+        });
+
+	}
+
+}
