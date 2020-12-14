749308782fe4a2527cdd3c0447d4cd92bd12b1b3
Syafiqjos <syafiqjos@gmail.com>
2020-12-14 23:27:40 +0700

    Sorry, fixed Alien error - syafiq

--- Collision Detection/src/id/ac/its/richard0017/syafiq0089/afifan0234/Alien.java
+++ Collision Detection/src/id/ac/its/richard0017/syafiq0089/afifan0234/Alien.java
@@ -1,6 +1,5 @@
 package id.ac.its.richard0017.syafiq0089.afifan0234;
 
-<<<<<<< HEAD
 public class Alien extends Sprite {
 
     private final int INITIAL_X = 400;
@@ -10,15 +9,6 @@ public class Alien extends Sprite {
 
         initAlien();
     }
-=======
-
-public class Alien extends Sprite {
-
-		private final int INITIAL_X = 400;
-	    
-	    public Alien(int x, int y) {
-	        super(x, y);
->>>>>>> eee7c9c2ad11e31973bc6e4003a1759a7aa0aa36
 
     private void initAlien() {
 
@@ -29,7 +19,6 @@ public class Alien extends Sprite {
     //Alien akan kembali ke screen sisi kanan setelah hilang melewati sisi kiri layar
     public void move() {
 
-<<<<<<< HEAD
         if (x < 0) {
             x = INITIAL_X;
         }
@@ -37,18 +26,3 @@ public class Alien extends Sprite {
         x -= 1;
     }
 }
-=======
-	        loadImage("src/resources/alien.png");
-	        getImageDimensions();
-	    }
-	    
-	    public void move() {
-
-	        if (x < 0) {
-	            x = INITIAL_X;
-	        }
-
-	        x -= 1;
-	    }
-}
->>>>>>> eee7c9c2ad11e31973bc6e4003a1759a7aa0aa36
