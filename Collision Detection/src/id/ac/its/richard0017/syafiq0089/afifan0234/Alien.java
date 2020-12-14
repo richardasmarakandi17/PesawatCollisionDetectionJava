77b73849df639ec98386fffd98a51af9a160532e
anakpayah <afifansyafaqi@ymail.com>
2020-12-14 19:27:03 +0700

    Memasukan Code Alien.java

---
+++ Collision Detection/src/id/ac/its/richard0017/syafiq0089/afifan0234/Alien.java
@@ -0,0 +1,31 @@
+package id.ac.its.richard0017.syafiq0089.afifan0234;
+
+public class Alien {
+
+	public class Alien extends Sprite {
+
+	    private final int INITIAL_X = 400;
+
+	    public Alien(int x, int y) {
+	        super(x, y);
+
+	        initAlien();
+	    }
+
+	    private void initAlien() {
+
+	        loadImage("src/resources/alien.png");
+	        getImageDimensions();
+	    }
+	    
+	    //Alien akan kembali ke screen sisi kanan setelah hilang melewati sisi kiri layar
+	    public void move() {
+
+	        if (x < 0) {
+	            x = INITIAL_X;
+	        }
+
+	        x -= 1;
+	    }
+	}
+}
